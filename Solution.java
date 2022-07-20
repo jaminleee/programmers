d01b935bab103b7245c50869b6039c8f1eaeb618
jaminleee <jamin_00@naver.com>
2022-07-20 22:51:17 +0900

    평균 구하기

--- Solution.java
+++ Solution.java
@@ -1,23 +1,13 @@
 class Solution {
-    public long solution(int a, int b) {
-        long answer = 0;
-        if (a > b) {
-            int temp = a;
-            a = b;
-            b = temp;
+    public double solution(int[] arr) {
+        double answer = 0;
+        int len = arr.length;
+        for (int i = 0; i < len; i++) {
+            answer += arr[i];
         }
-        for (int i = a; i <= b; i++) {
-            answer += i;
-        }
+
+        answer /= len;
         return answer;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        long answer = solution.solution(3, 5);
-
-        System.out.println(answer);
     }
 
 }
