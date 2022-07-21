121e0ce3019212942273c27536a3d9ee3366a019
jaminleee <91969458+jaminleee@users.noreply.github.com>
2022-07-22 02:18:17 +0900

    Add files via upload

---
+++ 테스트용.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] arr1 = { { 1, 2 }, { 2, 3 } };
+        int[][] arr2 = { { 3, 4 }, { 5, 6 } };
+        int[] arr = { 4, 3, 2, 1 };
+
+    }
+
+}
