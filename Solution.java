5b616fa51169aa839d2d8ed97ad1a9f65578a4e5
jaminleee <jamin_00@naver.com>
2022-07-22 02:10:38 +0900

    자릿수 더하기

--- Solution.java
+++ Solution.java
@@ -1,22 +1,17 @@
 class Solution {
-    public String solution(String s) {
-        String answer = "";
-        int len = s.length();
-        int index = len / 2;
-        if (len % 2 == 0) {
-            answer = s.substring(index - 1, index + 1);
-        } else {
-            answer = s.substring(index, index + 1);
-        }
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
         return answer;
     }
+
+}
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[][] arr1 = { { 1, 2 }, { 2, 3 } };
         int[][] arr2 = { { 3, 4 }, { 5, 6 } };
         int[] arr = { 4, 3, 2, 1 };
-        String answer = solution.solution("qswer");
+        int answer = solution.solution(123);
         System.out.println(answer);
 
     }
