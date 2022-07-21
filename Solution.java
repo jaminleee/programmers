0fab0232431a2aa51f20ea3d99c6666c3b7cffc5
jaminleee <jamin_00@naver.com>
2022-07-22 01:48:45 +0900

    가운데 글자 가져오기

--- Solution.java
+++ Solution.java
@@ -1,21 +1,12 @@
 class Solution {
-    public int[] solution(int[] arr) {
-        int[] answer = {};
-        int index = 0;
-        int len = arr.length;
-        int min = arr[0];
-        if (len == 1) {
-            answer[0] = -1;
+    public String solution(String s) {
+        String answer = "";
+        int len = s.length();
+        int index = len / 2;
+        if (len % 2 == 0) {
+            answer = s.substring(index - 1, index + 1);
         } else {
-            for (int i = 1; i < len; i++) {
-                if (min > arr[i]) {
-                    min = arr[i];
-                    index = i;
-                }
-            }
-            for (int i = index; i < len; i++) {
-                answer[i] = arr[i + 1];
-            }
+            answer = s.substring(index, index + 1);
         }
         return answer;
     }
@@ -25,8 +16,8 @@ class Solution {
         int[][] arr1 = { { 1, 2 }, { 2, 3 } };
         int[][] arr2 = { { 3, 4 }, { 5, 6 } };
         int[] arr = { 4, 3, 2, 1 };
-        int[] answer = solution.solution(arr);
-        System.out.println(arr[0]);
+        String answer = solution.solution("qswer");
+        System.out.println(answer);
 
     }
 
