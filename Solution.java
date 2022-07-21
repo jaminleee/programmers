2ddaa2aaae9ac43f41ff17cf77d5ddd3f44d80e3
jaminleee <jamin_00@naver.com>
2022-07-22 02:16:48 +0900

    커밋이 되나

--- Solution.java
+++ Solution.java
@@ -9,8 +9,6 @@ class Solution {
         int[][] arr1 = { { 1, 2 }, { 2, 3 } };
         int[][] arr2 = { { 3, 4 }, { 5, 6 } };
         int[] arr = { 4, 3, 2, 1 };
-        // int answer = solution.solution(123);
-        // System.out.println(answer);
 
     }
 
