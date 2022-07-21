668e6c2b968ce33486277141f12751fdeb468afb
jaminleee <jamin_00@naver.com>
2022-07-22 02:13:01 +0900

    테스트작업

--- Solution.java
+++ Solution.java
@@ -4,15 +4,13 @@ class Solution {
         return answer;
     }
 
-}
-
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[][] arr1 = { { 1, 2 }, { 2, 3 } };
         int[][] arr2 = { { 3, 4 }, { 5, 6 } };
         int[] arr = { 4, 3, 2, 1 };
-        int answer = solution.solution(123);
-        System.out.println(answer);
+        // int answer = solution.solution(123);
+        // System.out.println(answer);
 
     }
 
