4a72a6387a917896010f3a1a8f8b243a80c87010
jaminleee <jamin_00@naver.com>
2022-07-30 13:44:47 +0900

    나머지가 1이 되는 수

--- Solution.java
+++ Solution.java
@@ -1,14 +1,22 @@
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
+    public int solution(int n) {
+        int answer = 0;
+        for (answer = 2; answer < n; answer++) {
+            if (n % answer == 1) {
+                break;
+            }
+        }
+
         return answer;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[][] arr1 = { { 1, 2 }, { 2, 3 } };
-        int[][] arr2 = { { 3, 4 }, { 5, 6 } };
+        int[][] arr1 = { { 1 }, { 2 } };
+        int[][] arr2 = { { 3 }, { 5 } };
         int[] arr = { 4, 3, 2, 1 };
+        int[][] returntest = solution.solution(arr1, arr2);
+        System.out.println(returntest[0][0]);
 
     }
 
