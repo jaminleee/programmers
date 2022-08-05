c6437e683404213126d1863bd5a072e42ee1a1cb
jaminleee <jamin_00@naver.com>
2022-08-06 05:28:07 +0900

    정수 제곱근 판별

--- Solution.java
+++ Solution.java
@@ -2,26 +2,13 @@ import java.util.Arrays;
 import java.util.function.LongSupplier;
 
 class Solution {
-    public String solution(String s, int n) {
-        String answer = "";
-        int askii;
-        char arr[] = s.toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-            askii = (int) arr[i];
-            if (askii >= 65 && askii <= 90) {
-                askii += n;
-                if (askii > 90) {
-                    askii -= 26;
-                }
-            } else if (askii >= 97 && askii <= 122) {
-                askii += n;
-                if (askii > 122) {
-                    askii -= 26;
-                }
-            }
-            arr[i] = (char) askii;
-            answer += arr[i];
-
+    public long solution(long n) {
+        long answer = 0;
+        long sqrt = (long) Math.sqrt(n);
+        if (Math.pow(sqrt, 2) == n) {
+            answer += Math.pow(sqrt + 1, 2);
+        } else {
+            answer = -1;
         }
         return answer;
     }
@@ -31,7 +18,7 @@ class Solution {
         int[][] arr1 = { { 1 }, { 2 } };
         int[][] arr2 = { { 3 }, { 5 } };
         int[] arr = { 4, 3, 2, 1 };
-        String returntest = solution.solution("z", 1);
+        long returntest = solution.solution(121);
         System.out.println(returntest);
 
     }
