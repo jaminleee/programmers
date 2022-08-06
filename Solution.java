41b8d94bb29cc018547fbed9eb67b92c41589052
jaminleee <jamin_00@naver.com>
2022-08-06 20:44:36 +0900

    문자열 다루기 기본

--- Solution.java
+++ Solution.java
@@ -5,27 +5,19 @@ import java.util.function.LongSupplier;
 import javax.management.monitor.CounterMonitor;
 
 class Solution {
-    public int[] solution(int[] arr, int divisor) {
-        int[] answer = {};
-        int[] temp = new int[arr.length];
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % divisor == 0) {
-                temp[count] = arr[i];
-                count++;
+    public boolean solution(String s) {
+        boolean answer = true;
+        char ch[] = s.toCharArray();
+        if (s.length() == 4 || s.length() == 6) {
+            for (int i = 0; i < s.length(); i++) {
+                if ((int) ch[i] < 48 || (int) ch[i] > 57) {
+                    answer = false;
+                    break;
+                }
             }
-        }
-        if (count == 0) {
-            answer = new int[1];
-            answer[0] = -1;
         } else {
-            answer = new int[count];
-            for (int i = 0; i < count; i++) {
-                answer[i] = temp[i];
-            }
-            Arrays.sort(answer);
+            answer = false;
         }
-
         return answer;
     }
 
@@ -34,8 +26,8 @@ class Solution {
         int[][] arr1 = { { 1 }, { 2 } };
         int[][] arr2 = { { 3 }, { 5 } };
         int[] arr = { 3, 2, 6 };
-        int[] returntest = solution.solution(arr, 10);
-        System.out.println(returntest[1]);
+        boolean returntest = solution.solution("1234");
+        System.out.println(returntest);
 
     }
 
