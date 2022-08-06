8b6b79ad15c526535294f21df505918316c14c56
jaminleee <jamin_00@naver.com>
2022-08-06 20:08:16 +0900

    정수 내림차순으로 배치하기

--- Solution.java
+++ Solution.java
@@ -1,15 +1,18 @@
+import java.lang.invoke.VarHandle.AccessMode;
 import java.util.Arrays;
 import java.util.function.LongSupplier;
 
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        long sqrt = (long) Math.sqrt(n);
-        if (Math.pow(sqrt, 2) == n) {
-            answer += Math.pow(sqrt + 1, 2);
-        } else {
-            answer = -1;
+        String str = Long.toString(n);
+        String s = "";
+        char[] ch = str.toCharArray();
+        Arrays.sort(ch);
+        for (int i = ch.length - 1; i >= 0; i--) {
+            s += ch[i];
         }
+        answer = Long.parseLong(s);
         return answer;
     }
 
@@ -18,7 +21,7 @@ class Solution {
         int[][] arr1 = { { 1 }, { 2 } };
         int[][] arr2 = { { 3 }, { 5 } };
         int[] arr = { 4, 3, 2, 1 };
-        long returntest = solution.solution(121);
+        long returntest = solution.solution(118372);
         System.out.println(returntest);
 
     }
